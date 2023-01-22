e2ced560577b9728ec1f805eb67d788c662ea06e
JohnHozin <simple_protector@mail.ru>
2023-01-22 22:33:24 +0300

    fix hw_1

--- src/main/java/home_work_1/Task_1.java
+++ src/main/java/home_work_1/Task_1.java
@@ -10,14 +10,14 @@ public class Task_1 {
             arr[i] = rnd.nextInt(-999, 1000);
         }
 
-        int minValue = 0;
-        int maxValue = 0;
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             minValue = Math.min(minValue, arr[i]);
             maxValue = Math.max(maxValue, arr[i]);
         }
 
-        System.out.println("Максимальный элемент: " + minValue);
-        System.out.println("Минимальный элемент: " + maxValue);
+        System.out.println("Максимальный элемент: " + maxValue);
+        System.out.println("Минимальный элемент: " + minValue);
     }
 }
--- src/main/java/home_work_1/Task_3.java
+++ src/main/java/home_work_1/Task_3.java
@@ -6,25 +6,41 @@ import java.util.Random;
 public class Task_3 {
     public static void main(String[] args) {
         int val = 3;
-        int[] nums = new int[100];
+        int[] nums = new int[70];
         Random rnd = new Random();
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = rnd.nextInt(0, 6);
+            nums[i] = rnd.nextInt(0, 4);
         }
 
         System.out.println(Arrays.toString(nums));
 
+//        int rightIndex = nums.length - 1;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (i <= rightIndex) {
+//                if (nums[i] == val && nums[rightIndex] != val) {
+//                    nums[i] = nums[rightIndex];
+//                    nums[rightIndex] = val;
+//                }
+//                if (nums[rightIndex] == val) {
+//                    rightIndex--;
+//                    i--;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(nums));
+
+
         int rightIndex = nums.length - 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (i <= rightIndex) {
-                if (nums[i] == val && nums[rightIndex] != val) {
-                    nums[i] = nums[rightIndex];
-                    nums[rightIndex] = val;
-                }
-                if (nums[rightIndex] == val) {
+        for (int i = 0; i < rightIndex; i++) {
+            if (nums[i] == val) {
+                while (nums[rightIndex] == val) {
                     rightIndex--;
-                    i--;
+                    if (rightIndex <= i) {
+                        break;
+                    }
                 }
+                nums[i] = nums[rightIndex];
+                nums[rightIndex] = val;
             }
         }
         System.out.println(Arrays.toString(nums));
